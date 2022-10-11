2e92cf58af26dc560e70b33bcd219fce262e6517
Guilherme Luiz Argentino Silva <rrantz@gmail.com>
2022-10-11 18:43:29 -0300

    feature: melhoria no teste pra usar ResponseEntity

--- src/main/java/com/elumini/autorizador/application/rest/CartaoController.java
+++ src/main/java/com/elumini/autorizador/application/rest/CartaoController.java
@@ -1,7 +1,9 @@
 package com.elumini.autorizador.application.rest;
 
 import java.math.BigDecimal;
+import java.util.Map.Entry;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,18 +12,35 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.elumini.autorizador.application.request.CartaoRequest;
 import com.elumini.autorizador.application.response.CartaoResponse;
+import com.elumini.autorizador.domain.Cartao;
+import com.elumini.autorizador.domain.service.CartaoService;
+
+import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping("cartoes")
+@AllArgsConstructor
 public class CartaoController {
-	
+
+	private final CartaoService service;
+
 	@PostMapping
-	public CartaoResponse cria(CartaoRequest cartao) {
-		return null;
+	public ResponseEntity<CartaoResponse> cria(CartaoRequest cartao) {
+		Entry<Cartao, Boolean> result = service.cria(de(cartao));
+		return result.getValue() ? ResponseEntity.ok(de(result.getKey()))
+				: ResponseEntity.unprocessableEntity().body(de(result.getKey()));
 	}
-	
+
 	@GetMapping("{numeroCartao}")
 	public BigDecimal saldo(@PathVariable BigDecimal numeroCartao) {
+		return null;
+	}
+
+	private Cartao de(CartaoRequest cartao) {
+		return null;
+	}
+
+	private CartaoResponse de(Cartao cartao) {
 		return null;
 	}
 
