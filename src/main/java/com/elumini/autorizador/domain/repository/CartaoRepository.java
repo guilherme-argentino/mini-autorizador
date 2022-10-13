564b93028463c857580c0f3a70458d1ffec45b94
Guilherme Luiz Argentino Silva <rrantz@gmail.com>
2022-10-13 19:00:25 -0300

    feature: service impl (GREEN)

--- src/main/java/com/elumini/autorizador/domain/repository/CartaoRepository.java
+++ src/main/java/com/elumini/autorizador/domain/repository/CartaoRepository.java
@@ -5,10 +5,16 @@ import java.util.Optional;
 
 import com.elumini.autorizador.domain.Cartao;
 
+/**
+ * Port para gravação dos dados da entidade {@link Cartao} em um banco
+ * 
+ * @author Guilherme Luiz Argentino Silva
+ *
+ */
 public interface CartaoRepository {
-	
+
 	Optional<Cartao> findById(BigDecimal id);
-	
+
 	void save(Cartao cartao);
 
 }
--- src/main/java/com/elumini/autorizador/domain/service/CartaoService.java
+++ src/main/java/com/elumini/autorizador/domain/service/CartaoService.java
@@ -1,5 +1,6 @@
 package com.elumini.autorizador.domain.service;
 
+import java.util.AbstractMap;
 import java.util.Map.Entry;
 
 import com.elumini.autorizador.domain.Cartao;
@@ -7,10 +8,10 @@ import com.elumini.autorizador.domain.repository.CartaoRepository;
 
 /**
  * 
- * <h2> Serviço do domínio para {@link Cartao} </h2>
+ * <h2>Serviço do domínio para {@link Cartao}</h2>
  * 
- * IMPORTANTE: para tornar este serviço livre de código de infraestrutura,
- * nenhum framework que poderia ser aplicado como o Spring Boot ou Lombok foram
+ * IMPORTANTE: para tornar este serviço livre de código de infraestrutura, os
+ * frameworks que poderiam ser aplicados como o Spring Boot ou Lombok foram
  * descartados para respeitar os princípios da arquitetura hexagonal e DDD.
  * 
  * @author Guilherme Luiz Argentino Silva
@@ -18,7 +19,6 @@ import com.elumini.autorizador.domain.repository.CartaoRepository;
  */
 public class CartaoService {
 
-	@SuppressWarnings("unused")
 	private final CartaoRepository repository;
 
 	public CartaoService(CartaoRepository repository) {
@@ -27,11 +27,15 @@ public class CartaoService {
 	}
 
 	public Entry<Cartao, Boolean> cria(Cartao cartao) {
-		return null;
+		if (existe(cartao)) {
+			return new AbstractMap.SimpleEntry<Cartao, Boolean>(cartao, false);
+		}
+		repository.save(cartao);
+		return new AbstractMap.SimpleEntry<Cartao, Boolean>(cartao, true);
 	}
 
 	public Boolean existe(Cartao cartao) {
-		return null;
+		return repository.findById(cartao.getNumeroCartao()).isPresent();
 	}
 
 }
