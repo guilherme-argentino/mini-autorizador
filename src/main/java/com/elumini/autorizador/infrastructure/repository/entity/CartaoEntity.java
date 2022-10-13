ab5de2acf2f170060394786e314f7162c7b4c9c3
Guilherme Luiz Argentino Silva <rrantz@gmail.com>
2022-10-13 20:03:39 -0300

    feature: teste de persistence layer de "criar novo cartão" (RED)

--- src/main/java/com/elumini/autorizador/infrastructure/repository/entity/CartaoEntity.java
+++ src/main/java/com/elumini/autorizador/infrastructure/repository/entity/CartaoEntity.java
@@ -7,11 +7,13 @@ import javax.persistence.Id;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.MODULE)
 public class CartaoEntity {
